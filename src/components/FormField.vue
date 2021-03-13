3e45867e7266e1a80f8bba186129f30a410f6258
Ocholla-T <tonnyocholla8@gmail.com>
2021-03-13 17:08:17 +0300

    added styling for submit button. Not added mapping to submit event

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -1,12 +1,14 @@
 <template>
-  <form action="" @submit.prevent="onSubmit">
+  <form action="" @submit.prevent="onSubmit" ref="form">
     <div class="form__group field" v-for="(label, index) in labelName" :key="index">
       <input ref="inputElements" type="input" class="form__field" placeholder="Name" name="name" id="name" v-model="inputValue[index]" required />
       <label for="name" class="form__label">{{ label.placeholder }}</label>
     </div>
 
-    <button type="submit">
-      <img src="../assets/submit&add.png" alt="submit" />
+    <button class="submit__button">
+      <span class="submit">Submit </span>
+      <span class="loading"><i class="fas fa-sync fa-2x"></i></span>
+      <span class="check"><i class="fas fa-check"></i></span>
     </button>
   </form>
 </template>
@@ -23,28 +25,35 @@ export default {
   },
 
   methods: {
-    onSubmit() {
+    onSubmit: function() {
+      //object that maps input data to their respective fields
       const devices = {
         device: this.inputValue[0],
         buyingPrice: this.inputValue[1],
         averageLifespan: this.inputValue[2],
         yearsInUse: this.inputValue[3],
       };
+
+      //firebase accepts data as objects
       createDevice(devices).then(() => {
         console.log("added to database!");
       });
+
+      this.inputValue = [];
     },
   },
 };
 </script>
-
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap");
 
+$base-duration: 500ms;
 $primary: #11998e;
 $secondary: #38ef7d;
 $white: #fff;
 $gray: #9b9b9b;
+$color-2: #3498db;
+$color-3: #2ecc71;
 $font: "Lato", sans-serif;
 
 form {
@@ -54,17 +63,17 @@ form {
 .form__group {
   position: relative;
   padding: 15px 0 0;
-  margin-top: 10px;
+  margin: 20px auto;
   width: 100%;
 }
 
 .form__field {
   font-family: $font;
-  width: 100%;
+  width: 75%;
   border: 0;
   border-bottom: 2px solid $gray;
   outline: 0;
-  font-size: 1.3rem;
+  font-size: 1rem;
   padding: 10px 0;
   background: transparent;
   transition: border-color 0.2s;
@@ -114,9 +123,132 @@ form {
 }
 
 /*button styling */
-button {
-  margin: 1.5rem auto auto auto;
-  background-color: $white;
+
+.submit__button {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2.625rem;
+  width: 12.5rem;
+  padding: 1.5rem 3.125rem;
   border: none;
+  border-radius: 0.3125rem;
+  box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.2);
+  background-color: #1e90ff;
+  color: $white;
+  font-weight: 600;
+  font-size: medium;
+  overflow: hidden;
+  text-transform: uppercase;
+
+  &:before {
+    position: absolute;
+    content: "";
+    bottom: 0;
+    left: 0;
+    width: 0%;
+    height: 100%;
+    background-color: lighten($color-3, 10%);
+  }
+}
+
+span {
+  position: absolute;
+  line-height: 0;
+
+  i {
+    transform-origin: center center;
+  }
+
+  &:nth-of-type(1) {
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  &:nth-of-type(2) {
+    top: 100%;
+    transform: translateY(0%);
+    font-size: 24px;
+  }
+
+  &:nth-of-type(3) {
+    display: none;
+  }
+}
+
+.active {
+  background-color: $color-3;
+
+  &:before {
+    width: 100%;
+    transition: width 3s linear;
+  }
+
+  span {
+    &:nth-of-type(1) {
+      top: -100%;
+      transform: translateY(-50%);
+    }
+
+    &:nth-of-type(2) {
+      top: 50%;
+      transform: translateY(-50%);
+
+      i {
+        animation: loading $base-duration linear infinite;
+      }
+    }
+
+    &:nth-of-type(3) {
+      display: none;
+    }
+  }
+}
+
+.finished {
+  background-color: lighten($color-3, 10%);
+
+  .submit {
+    display: none;
+  }
+
+  .loading {
+    display: none;
+  }
+
+  .check {
+    display: block !important;
+    font-size: 24px;
+    animation: scale 0.5s linear;
+
+    i {
+      transform-origin: center center;
+    }
+  }
+}
+
+@keyframes loading {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes scale {
+  0% {
+    transform: scale(10);
+  }
+  50% {
+    transform: scale(0.2);
+  }
+  70% {
+    transform: scale(1.2);
+  }
+  90% {
+    transform: scale(0.7);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
