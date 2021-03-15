bdbd9e3cf6aecd81e318c0d99c720a2b5591d15e
Ocholla-T <tonnyocholla8@gmail.com>
2021-03-15 08:51:05 +0300

    added RightSectionOutput component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="home">
     <LeftSectionInputs />
+    <RightSectionOutput />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import LeftSectionInputs from "@/components/LeftSectionInputs.vue";
+import RightSectionOutput from "@/components/RightSectionOutput.vue";
 
 export default {
   name: "Home",
   components: {
     LeftSectionInputs,
+    RightSectionOutput,
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .home {
+  display: flex;
+  justify-content: space-between;
   margin: 0 3rem;
 }
 </style>
